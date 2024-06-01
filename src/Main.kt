a6df96be384933e3e68231eb54c75259ac49c623
thawan-xyz <thawan.xyz@gmail.com>
2024-06-01 12:30:43 -0300

    accepted

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val numberOfEvents = readln().toInt()
+    val listOfEvents = readln().split(" ").map { x -> x.toInt() }
+
+    var numberOfPoliceOfficers = 0
+    var numberOfUntreatedCrimes = 0
+
+    for (currentEvent in listOfEvents) {
+        if (currentEvent != -1) {
+            numberOfPoliceOfficers += currentEvent
+        }
+        else if (numberOfPoliceOfficers != 0) {
+            numberOfPoliceOfficers -= 1
+        }
+        else {
+            numberOfUntreatedCrimes += 1
+        }
+    }
+
+    println(numberOfUntreatedCrimes)
+}
